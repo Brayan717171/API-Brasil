64bfb56cfdcdaac3713d91ee38b694a64345effc
BRAYAN ALVES DA SILVA CLARA <25203616@CFP127ALUNO.LOCAL>
2026-04-10 08:32:24 -0300

    concluindo e colocando as funcoes e documento que faltavam subir

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const funcoes = require('../dados/funcao');
+const funcoes = require('./dados/funcao');
 
 const app = express();
 const PORT = 3000;
